d00f6b634d9fc5113c7695c682c8e89f5dfc2a33
TARO <shun.ichiro.tanaka16@gmail.com>
2022-03-19 17:59:43 +0900

    現在の西暦をを自動にする

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -1,7 +1,10 @@
-import React from 'react';
 import { NavigationDots, SocialMedia } from '../components';
 
+
+
 const AppWrap = (Component, idName, classNames) => function HOC() {
+  const year = new Date().getFullYear()
+
   return (
     <div id={idName} className={`app__container ${classNames}`}>
       <SocialMedia />
@@ -9,7 +12,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
         <Component />
 
         <div className="copyright">
-          <p className="p-text">@2020 MICHAEL</p>
+          <p className="p-text">@ {year}  Shun-ichiro</p>
           <p className="p-text">All rights reserved</p>
         </div>
       </div>
